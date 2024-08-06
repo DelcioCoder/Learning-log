5cab96c8e0081a0e3d17192333e63b1d882f4254
DelcioCoder <armandodelcio40@gmail.com>
2024-08-06 12:55:30 +0100

    adicionei a funcionalidade de mensagem de sucesso

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Topic,Entry
-from .forms import TopicForm,EntryForm 
-from django.http import HttpResponseRedirect,Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-# Create your views here.
 
+# Create your views here.
 
 def index(request):
     return render(request, 'learning_logs/index.html')
 
 @login_required
 def topics(request):
-    topics = Topic.objects.filter(owner = request.user).order_by("date_added")
-    context = {'topics':topics}
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
+    status = request.GET.get('status')
+    context = {'topics': topics, 'status': status}
     return render(request, 'learning_logs/topics.html', context)
 
 @login_required
@@ -22,27 +23,27 @@ def topic(request, topic_id):
     if topic.owner != request.user:
         raise Http404
     entries = topic.entry_set.order_by('-date_added')
-    context = {'topic':topic, 'entries':entries}
+    context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 @login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
+        
     else:
         form = TopicForm(data=request.POST)
-        if form.is_valid:
+        if form.is_valid():
             new_topic = form.save(commit=False)
-            new_topic.owner  = request.user
+            new_topic.owner = request.user
             new_topic.save()
-            return HttpResponseRedirect(reverse('learning_logs:topics'))
-    context = {'form':form}
+            return HttpResponseRedirect(reverse('learning_logs:topics') + '?status=1')
+    context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
-
 
 @login_required
 def new_entry(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     if topic.owner != request.user:
         raise Http404
     if request.method != 'POST':
@@ -54,9 +55,8 @@ def new_entry(request, topic_id):
             new_entry.topic = topic
             new_entry.save()
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
-    context = {'topic':topic, 'form':form}
+    context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
-
 
 @login_required
 def edit_entry(request, entry_id):
@@ -71,19 +71,14 @@ def edit_entry(request, entry_id):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
-        
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
 
-
-
+@login_required
 def delete_entry(request, entry_id):
     entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     if topic.owner != request.user:
         raise Http404
-    else:
-        entry.delete()
-        return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
-        
-
+    entry.delete()
+    return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
