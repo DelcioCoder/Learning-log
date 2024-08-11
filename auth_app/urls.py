75a9ef126fa7dfec57ebf357017f00e2f4674f00
DelcioCoder <armandodelcio40@gmail.com>
2024-08-11 20:15:25 +0100

    I implemented the password change feature

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,5 +6,6 @@ app_name = "auth_app"
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth_app/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('changepassword/', views.changepassword, name='changepassword')
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth import logout,authenticate,login
+from django.contrib.auth import logout,authenticate,login,update_session_auth_hash
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse 
+from django.contrib.auth.models import User
+
+
+from .forms import CustomPasswordChangeForm
 
 # Create your views here.
 
@@ -24,3 +28,26 @@ def register(request):
             return HttpResponseRedirect(reverse('learning_logs:index'))
     context = {'form':form}
     return render(request, 'auth_app/register.html', context)
+
+
+
+def changepassword(request):
+    if request.method == 'POST':
+        form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user) # actualizar a sessão para manter o usuario logado após a alteração da senha.
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    else:
+        form = CustomPasswordChangeForm(user=request.user)
+
+    context = {'form': form}
+
+        
+    
+    return render(request, 'auth_app/changepassword.html',context)
+
+
+
+
+
